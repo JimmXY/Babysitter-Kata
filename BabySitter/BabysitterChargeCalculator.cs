96ca8edd052f4e2c565492fcc64cb9744e4bf88b
Hariharan Arunachalam <hariharana87@gmail.com>
2016-09-27 11:40:36 -0500

    Implementation for passing one hour past bedtime added

--- BabySitter/BabysitterChargeCalculator.cs
+++ BabySitter/BabysitterChargeCalculator.cs
@@ -28,9 +28,9 @@ namespace BabySitter
         /// Constructor for the baby sitter charge calculator with 
         /// a default bedtime of 8 PM.
         /// </summary>
-        public BabysitterChargeCalculator(): this(GetTimeMerged(20,0))
+        public BabysitterChargeCalculator() : this(GetTimeMerged(20, 0))
         {
-            
+
         }
 
 
@@ -42,11 +42,22 @@ namespace BabySitter
         /// <returns>The calculated charge the customer owes the baby sitter for the job</returns>
         public int Calculate(DateTime startTime, DateTime endTime)
         {
-            // compute the number of hours between end and start 
-            int hourDiff = endTime.Subtract(startTime).Hours;
-            Debug.Print("{0} to {1} = {2} hours", startTime, endTime, hourDiff);
-            // calculated cost is the diff in hours multiplied by 12
-            return 12 * hourDiff;
+            // if end time is after bedtime, calculate differently
+            if (endTime > BedTime)
+            {
+                int hoursBeforeBedTime = BedTime.Subtract(startTime).Hours;
+                int hoursAfterBedTime = endTime.Subtract(BedTime).Hours;
+                // calculated cost is $12/h till bedtime and then $8/h
+                return 12 * hoursBeforeBedTime + 8 * hoursAfterBedTime;
+            }
+            else
+            {
+                // compute the number of hours between end and start 
+                int hourDiff = endTime.Subtract(startTime).Hours;
+                Debug.Print("{0} to {1} = {2} hours", startTime, endTime, hourDiff);
+                // calculated cost is the diff in hours multiplied by 12
+                return 12 * hourDiff;
+            }
         }
 
         /// <summary>
