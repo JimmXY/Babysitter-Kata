37e955d2d20e2532e1ca517981fdf52688723ba7
Hariharan Arunachalam <hariharana87@gmail.com>
2016-09-27 10:49:25 -0500

    Failing test for one hour of job from 5 pm to 6 pm added.

--- BabySitter/BabysitterChargeCalculator.cs
+++ BabySitter/BabysitterChargeCalculator.cs
@@ -8,5 +8,17 @@ namespace BabySitter
 {
     public class BabysitterChargeCalculator
     {
+
+        /// <summary>
+        /// Computes and returns the sitter charge based on the start and end time of the sitter's job.
+        /// </summary>
+        /// <param name="startTime">The time of day the sitter started job</param>
+        /// <param name="endTime">The time of the day the stter's job ended</param>
+        /// <returns>The calculated charge the customer owes the baby sitter for the job</returns>
+        public int Calculate(DateTime startTime, DateTime endTime)
+        {
+
+            return 0;
+        }
     }
 }
--- BabySitterUnitTests/BabySitterTests.cs
+++ BabySitterUnitTests/BabySitterTests.cs
@@ -20,6 +20,18 @@ namespace BabySitterUnitTests
         }
 
         /// <summary>
+        /// Helper function to create time of day DateTimes
+        /// </summary>
+        /// <param name="hour">The hour value of the time of day - in 24 hour format</param>
+        /// <param name="minute">The minute value in the hour</param>
+        /// <returns></returns>
+        private DateTime GetTimeMerged(int hour, int minute)
+        {
+            DateTime timeSplicedDate = new DateTime(DateTime.Today.Year, DateTime.Today.Month, DateTime.Today.Day, hour, minute, 0);
+            return timeSplicedDate;
+        }
+
+        /// <summary>
         /// Simple first test to get things moving. Makes sure that the constructor is being called without any issues.
         /// This test is irrelevant in most cases - but is included for the first step.
         /// </summary>
@@ -27,6 +39,15 @@ namespace BabySitterUnitTests
         public void IsBabySitterInitializedTest()
         {
             Assert.IsNotNull(sitterCalculator);
+        }
+
+        [TestMethod]
+        public void WhenBabySitterWorksForOneHourBeforeBedtimeGetsPaid12USD()
+        {
+            DateTime startTime = GetTimeMerged(17, 0);
+            DateTime endTime = GetTimeMerged(18, 0);
+            // When starting at 5 PM, working for one hour till 6 PM, must get paid $12
+            Assert.AreEqual(12, sitterCalculator.Calculate(startTime, endTime));
         }
     }
 }
