e4d2a028704436a1c1285f6d205b0b8de3e5ca51
Hariharan Arunachalam <hariharana87@gmail.com>
2016-09-27 10:33:33 -0500

    First basic null check test added - checks for setup completion.

---
+++ BabySitterUnitTests/BabySitterTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BabySitter;
+
+namespace BabySitterUnitTests
+{
+    [TestClass]
+    public class BabySitterTests
+    {
+
+        /// <summary>
+        /// Reference to the sitter class to be used for testing
+        /// </summary>
+        BabysitterChargeCalculator sitterCalculator;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            sitterCalculator = new BabysitterChargeCalculator();
+        }
+
+        /// <summary>
+        /// Simple first test to get things moving. Makes sure that the constructor is being called without any issues.
+        /// This test is irrelevant in most cases - but is included for the first step.
+        /// </summary>
+        [TestMethod]
+        public void IsBabySitterInitializedTest()
+        {
+            Assert.IsNotNull(sitterCalculator);
+        }
+    }
+}
